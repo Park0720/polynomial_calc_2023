71a1f388a8eaa8e38f2edee7108b32f6ba8602d9
Park0720 <ppmm98@naver.com>
2023-03-10 16:45:08 +0900

    연산 여러개, 빼기 개선

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,16 +8,10 @@ public class Main {
 class Calc {
     static int run(String exp) {
         int answer = 0;
-        if (exp.contains("+")) {
-            String[] bits = exp.split(" \\+ ");
-            for (int i = 0; i < bits.length; i++) {
-                answer += Integer.parseInt(bits[i]);
-            }
-        } else if (exp.contains("-")) {
-            String[] bits = exp.split(" - ");
-            int a = Integer.parseInt(bits[0]);
-            int b = Integer.parseInt(bits[1]);
-            return a - b;
+        exp = exp.replaceAll("- ", "+ -");
+        String[] bits = exp.split(" \\+ ");
+        for (int i = 0; i < bits.length; i++) {
+            answer += Integer.parseInt(bits[i]);
         }
         return answer;
     }
