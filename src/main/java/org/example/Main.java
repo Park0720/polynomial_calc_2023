d5c9f8e12f6e1b919c656b7555d2859a8a44b9c5
Park0720 <ppmm98@naver.com>
2023-03-10 15:45:09 +0900

    더하기 공식

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    }
+}
+
+class Calc {
+    static int run(String exp) {
+
+            String[] bits = exp.split(" \\+ ");
+            int a = Integer.parseInt(bits[0]);
+            int b = Integer.parseInt(bits[1]);
+            return a + b;
     }
 }
